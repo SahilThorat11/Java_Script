71a9ee94ca2ca46ec8862ef6942c678769c4848a
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-21 18:15:10 +0530

    Find Largest

---
+++ Assignments/Assignment_05/Program5_5.js
@@ -0,0 +1,25 @@
+
+function FindLargest(iNo1, iNo2, iNo3)
+{
+    if (iNo3 > iNo2) 
+    {
+        console.log(`${iNo3} is largest number.`);
+    }
+    else if (iNo2 > iNo1) 
+    {
+        console.log(`${iNo2} is largest number.`);
+    }
+    else
+    {
+        console.log(`${iNo1} is largest number.`);
+    }
+}
+
+function main()
+{
+    let iValue1 = 1, iValue2 = 21, iValue3 = 1;
+
+    FindLargest(iValue1, iValue2, iValue3);
+}
+
+main();
