4550eead6c09d94181ffe23206a5fc075e78ade7
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-21 17:37:55 +0530

    Find Maximum

---
+++ Assignments/Assignment_05/Program5_2.js
@@ -0,0 +1,23 @@
+
+function FindMax(iNo1, iNo2)
+{
+    if(iNo1 > iNo2)
+    {
+        return iNo1;
+    }
+    else
+    {
+        return iNo2;
+    }
+}
+
+function main()
+{
+    let iValue1 = 11, iValue2 = 13, iRet = 0;
+
+    iRet = FindMax(iValue1, iValue2);
+
+    console.log(`Maximum between ${iValue1} and ${iValue2} is : ${iRet}`);
+}
+
+main();
